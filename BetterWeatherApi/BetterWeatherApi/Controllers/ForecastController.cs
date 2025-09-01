d4e9e6248a056b9dd7d414754cbfc62b7ae1ad89
yvano123 <yvanovdwal@gmail.com>
2025-09-01 17:46:37 +0200

    Added extra data to weather response

--- BetterWeatherApi/BetterWeatherApi/Controllers/ForecastController.cs
+++ BetterWeatherApi/BetterWeatherApi/Controllers/ForecastController.cs
@@ -1,23 +1,39 @@
-﻿using BetterWeatherApi.Logic.Services;
+﻿using AutoMapper;
+using BetterWeatherApi.Domain.Models.Forecast.Responses;
+using BetterWeatherApi.Logic.Services;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BetterWeatherApi.Web.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("CorsPolicy")]
     public class ForecastController : ControllerBase
     {
         private readonly IWeatherService _weatherService;
-        public ForecastController(IWeatherService weatherService) 
+        private readonly IMapper _mapper;
+
+        public ForecastController(IWeatherService weatherService, IMapper mapper) 
         {
             _weatherService = weatherService;
+            _mapper = mapper;
         }
         [HttpGet]
         public async Task<IActionResult> Get(string location)
         {
-            var data = await _weatherService.GetWeekForecast(location);
+            var weatherData = await _weatherService.GetWeekForecast(location);
+            if (weatherData != null)
+            {
+                var response = _mapper.Map<List<DailyForecastResponseModel>>(weatherData);
 
-            return Ok(data);
+                var host = $"{Request.Scheme}://{Request.Host}";
+                response = _weatherService.InsertAdditionalWeatherData(response, host);
+                return Ok(response);
+            }
+
+            return NotFound();
+
         }
     }
 }

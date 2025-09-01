dac9e2b69fdd8802650a8eb52eb03ed2edf35c97
yvano123 <yvanovdwal@gmail.com>
2025-09-01 17:46:17 +0200

    Added a cors policy to fix access controll allow origin header

--- BetterWeatherApi/BetterWeatherApi/Program.cs
+++ BetterWeatherApi/BetterWeatherApi/Program.cs
@@ -6,6 +6,16 @@ using BetterWeatherApi.Domain.Mappers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+options.AddPolicy("CorsPolicy", policy =>
+{
+    policy.AllowAnyHeader()
+          .AllowAnyMethod()
+          .AllowAnyOrigin();
+});
+});
+
 {
     builder.Services.AddScoped<IWeatherClient>(prov =>
     {
@@ -20,15 +30,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 
+
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
 
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAuthorization();
 
+app.UseCors("CorsPolicy");
 app.MapControllers();
 
 app.Run();
